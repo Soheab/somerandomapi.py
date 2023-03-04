461e4d4767a3d51eb2cf38590f5ace1de7a1fcd9
Soheab_ <33902984+Soheab@users.noreply.github.com>
2023-03-04 17:53:10 +0100

    Add chatbot.py example

---
+++ examples/chatbot.py
@@ -0,0 +1,24 @@
+import asyncio
+
+# import the library
+import somerandomapi
+
+
+# create a client
+sr_api = somerandomapi.Client(key="your-api-key-here")
+
+
+# define a function to use the chatbot
+async def chat():
+    message = input("What do you want to say to the chatbot? ")
+    # send a message to the chatbot
+    result = await sr_api.chatbot(message)
+    # or use it as a context manager
+    # async with sr_api.chatbot(client=sr_api) as chatbot:
+    #     result = await chatbot.send(message)
+    # print the message
+    print(f"Reponse to {result.message}: {result.response}")
+
+
+# run the function
+asyncio.run(chat())
