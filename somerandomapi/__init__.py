0c7d04b9066d50848eddde45c04f0ddec6cc9428
Soheab_ <33902984+Soheab@users.noreply.github.com>
2023-07-19 16:50:03 +0200

    Fix exceptions not being accessed from main namespace

--- somerandomapi/__init__.py
+++ somerandomapi/__init__.py
@@ -7,8 +7,9 @@ A maintained Python wrapper for the Somerandomapi API.
 from .clients import *
 from .enums import *
 from .models import *
+from .errors import *
 
 
-__version__ = "0.0.4a"
+__version__ = "0.0.5a"
 __author__ = "Soheab"
 __license__ = "MPL-2.0"
