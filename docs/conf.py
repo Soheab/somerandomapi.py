eef169c1874055973507344168147123657e245a
Soheab_ <33902984+Soheab@users.noreply.github.com>
2024-05-16 21:33:53 +0200

    Switch docs theme to Furo

--- docs/conf.py
+++ docs/conf.py
@@ -43,7 +43,7 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinx_rtd_theme"
+html_theme = "furo"
 # These folders are copied to the documentation's HTML output
 html_static_path = ["_static"]
 
@@ -52,12 +52,7 @@ html_static_path = ["_static"]
 html_css_files = [
     "css/custom.css",
 ]
-html_theme_options = {
-    "collapse_navigation": True,
-    "sticky_navigation": True,
-    "navigation_depth": 2,
-    "titles_only": False,
-}
+
 autodoc_default_options = {
     "members": True,
     #'undoc-members': False,
